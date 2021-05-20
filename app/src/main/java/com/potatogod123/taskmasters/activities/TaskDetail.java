8509a29ea84332ad3ae38215689f4cf41b7be0dd
Cristian Robles <formymoment12@gmail.com>
2021-05-20 00:00:20 -0500

    Added ads functionality merge with a pleasent user experience!

--- app/src/main/java/com/potatogod123/taskmasters/activities/TaskDetail.java
+++ app/src/main/java/com/potatogod123/taskmasters/activities/TaskDetail.java
@@ -1,26 +1,41 @@
 package com.potatogod123.taskmasters.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.amplifyframework.core.Amplify;
+import com.google.android.gms.ads.AdError;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.potatogod123.taskmasters.R;
 
 import java.io.File;
 import java.util.Locale;
 
 public class TaskDetail extends AppCompatActivity {
+    private InterstitialAd mInterstitialAd;
+
+    private final String TAG = "potatogod123.TaskDetail";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
-
+        configureFullAdd();
 
     }
 
@@ -62,5 +77,73 @@ public class TaskDetail extends AppCompatActivity {
                     }
             );
         }
+
     }
+
+
+    @Override
+    protected  void onStop(){
+        super.onStop();
+
+        if(mInterstitialAd!=null){
+            mInterstitialAd.show(TaskDetail.this);
+        }
+
+    }
+
+
+
+    private void configureFullAdd(){
+        MobileAds.initialize(getApplicationContext(), new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+            }
+        });
+        String testAddString = "ca-app-pub-3940256099942544/1033173712";
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        InterstitialAd.load(getApplicationContext(), testAddString, adRequest, new InterstitialAdLoadCallback() {
+            @Override
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                super.onAdLoaded(interstitialAd);
+                mInterstitialAd=interstitialAd;
+                populateAd();
+                Log.i(TAG, "onAdLoaded: it is loaded?"+interstitialAd.getResponseInfo());
+            }
+
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                super.onAdFailedToLoad(loadAdError);
+                mInterstitialAd=null;
+                Log.i(TAG, "onAdFailedToLoad: "+loadAdError.getMessage());
+            }
+        });
+
+
+
+
+    }
+    private void populateAd(){
+        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+            @Override
+            public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                super.onAdFailedToShowFullScreenContent(adError);
+                Log.d(TAG, "onAdFailedToShowFullScreenContent: -> "+adError.getMessage());
+            }
+
+            @Override
+            public void onAdShowedFullScreenContent() {
+                super.onAdShowedFullScreenContent();
+                Log.i(TAG, "onAdShowedFullScreenContent: ad was shown!");
+                mInterstitialAd=null;
+            }
+
+            @Override
+            public void onAdDismissedFullScreenContent() {
+                super.onAdDismissedFullScreenContent();
+                Log.i(TAG, "onAdDismissedFullScreenContent: ad was dimissed");
+            }
+        });
+    }
+
 }
